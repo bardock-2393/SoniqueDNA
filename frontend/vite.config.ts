7fb6d96e5109c6318bbcd1b1d227d69dd4185e56
Deep santoshwar <deepsantoshwar7@gmail.com>
2025-08-01 19:04:24 +0530

    without cps
    Signed-off-by: Deep santoshwar <deepsantoshwar7@gmail.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,9 +7,6 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
-    headers: {
-      'Content-Security-Policy': "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.spotify.com https://api.spotify.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https:; connect-src 'self' https://accounts.spotify.com https://api.spotify.com http://15.207.204.90:5500 https://15.207.204.90:5500;"
-    }
   },
   plugins: [
     react(),
